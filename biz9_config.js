9371e0f05d183af7d305e2b6d1f9dd462945047b
think2 <think2@localhost.localdomain>
2024-12-23 17:28:25 -0500

    best

--- biz9_config.js
+++ biz9_config.js
@@ -1,4 +1,4 @@
-exports.VERSION="1.0.15"
+exports.VERSION="1.0.16"
 exports.TITLE='BiZ9-Utility'
 exports.REPO='git@github.com:biz9framework/biz9-utility.git'
-exports.BRANCH='1.0.13'
+exports.BRANCH='main'
